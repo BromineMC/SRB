6c67a51aa13a395acecb8c2d86ef08d58bf09654
VidTu <imvidtu@proton.me>
2023-10-18 02:16:16 +0300

    chore: Bump to Java 21

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "ru.brominemc.srb.core"
-java.toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+java.toolchain.languageVersion.set(JavaLanguageVersion.of(21))
 
 repositories {
     mavenCentral()
@@ -20,7 +20,7 @@ dependencies {
 
 tasks.withType<JavaCompile> {
     options.encoding = "UTF-8"
-    options.release.set(17)
+    options.release.set(21)
 }
 
 tasks.withType<Jar> {
